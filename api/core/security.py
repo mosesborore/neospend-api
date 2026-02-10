ff9d8d8e013ef4d9f6444134832adf7f4786b683
Moses Borore <developerborore@gmail.com>
2026-02-10 18:03:35 +0300

    Remove deprecated datetime.utcnow

--- api/core/security.py
+++ api/core/security.py
@@ -24,7 +24,7 @@ password_hash = PasswordHash.recommended()
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
     """Create JWT access token"""
     to_encode = data.copy()
-    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=EXPIRE_MINUTES))
+    expire = datetime.now() + (expires_delta or timedelta(minutes=EXPIRE_MINUTES))
 
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
