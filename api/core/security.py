21d7d981dcc6fe6fab0acefcb477980fb5d67917
Moses Borore <mosesbororekinuthia@gmail.com>
2026-03-04 08:56:00 +0300

    Add get auth token from cookie function

--- api/core/security.py
+++ api/core/security.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 
-from fastapi import Depends, HTTPException, status
+from fastapi import Depends, HTTPException, Request, status
 from fastapi.security import OAuth2PasswordBearer
 from pwdlib import PasswordHash
 from pydantic import BaseModel
@@ -52,16 +52,47 @@ def authenticate_user(*, email: str, password: str, session: Session):
     return user
 
 
+async def get_current_user_cookie(request: Request, session: Session = Depends(get_session)):
+    # add CSRF token checking, or use middleware
+    token = request.cookies.get("auth_token")
+
+    try:
+        if not token:
+            raise credentials_exception
+
+        access_token = AccessToken(token=token)
+
+        user_id = access_token.get(settings.USER_ID_CLAIM)
+
+        if user_id is None:
+            raise credentials_exception
+
+        token_data = TokenData(user_id=int(user_id))
+
+    except TokenError as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=f"Could not validate credentials: {str(e)}",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    # get the user from db
+    user = get_user({"id": token_data.user_id}, session=session)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
 async def get_current_user(
     token: Annotated[str, Depends(oauth2_scheme)], session: Session = Depends(get_session)
 ):
     try:
-        access_token = AccessToken(token)
+        access_token = AccessToken(token=token)
 
-        user_id: str = access_token.get(settings.USER_ID_CLAIM)
+        user_id = access_token.get(settings.USER_ID_CLAIM)
         if user_id is None:
             raise credentials_exception
-        token_data = TokenData(user_id=user_id)
+        token_data = TokenData(user_id=int(user_id))
 
     except TokenError as e:
         raise HTTPException(
